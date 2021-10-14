aee1dfee66fcedfffea4a89d5f2b506d3cbbc89b
durgeshahire07 <durgeshahire07@gmail.com>
2021-10-15 00:44:20 +0530

    added readme file

---
+++ README.md
@@ -0,0 +1,26 @@
+# Time-Complexity-Extension
+A browser extension that gives the time complexity of various random algorithms everytime you open the extension or click on the Next button while using it.
+
+<img src="./imgs/screenshot.png" alt="screenshot" width="100%">
+
+### How to install the unpacked extension in Chrome ?
+
+- Go to **settings** chrome
+
+<img src="./imgs/settings.png" alt="settings chrome" width="100%">
+
+- Click on the button **"extension"**
+
+<img src="./imgs/extension.png" alt="settings chrome" width="100%">
+
+- Activate **developer mode**
+
+<img src="./imgs/developerMode.png" alt="settings chrome" width="100%">
+
+- Click on the button to load **extension** folder
+
+<img src="./imgs/load.png" alt="settings chrome" width="100%">
+
+- Load the folder
+
+<img src="./imgs/folde.png" alt="settings chrome" width="100%">
--- script.js
+++ script.js
@@ -3,10 +3,17 @@ const url = 'http://localhost:3000/posts';
 fetch(url)
 .then(res => res.json())
 .then(data=>{
-    console.log(data[0]);
+    // console.log(data[0]);
+    // if(data[0].note){
+    //     document.getElementById("note-text").innerText = `NOTE : ${data[0].note}` 
+    // }
+    // else{
+    //     document.getElementById("note-text").style.visibility = "hidden"; 
+    // }
     document.getElementById("algo-text").innerText = `Do you know the Time Complexity of ${data[0].title} ?` 
     document.getElementById("avg-time-comp-text").innerText = data[0].avg_time
     document.getElementById("best-time-comp-text").innerText = data[0].best_time
+    document.getElementById("worst-time-comp-text").innerText = data[0].worst_time
 })
 .catch(err => err);
 
@@ -18,10 +25,18 @@ function myFunction() {
     return result.json();
     })
     .then(data=>{
-        console.log(data[0]);
+        // console.log(data[0]);
+        // if(data[0].note){
+        //     document.getElementById("note-text").innerText = `NOTE : ${data[0].note}` 
+        // }
+        // else{
+        //     document.getElementById("note-text").style.visibility = "hidden";
+        //     // document.getElementById("note-text").style.visibility = "hidden";
+        // }
         document.getElementById("algo-text").innerText = `Do you know the Time Complexity of ${data[0].title} ?` 
         document.getElementById("avg-time-comp-text").innerText = data[0].avg_time
         document.getElementById("best-time-comp-text").innerText = data[0].best_time
+        document.getElementById("worst-time-comp-text").innerText = data[0].worst_time
     })
 
 }

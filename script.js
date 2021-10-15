6a5d9bda9be9ec4b3149a7a5b01d0d9bf0f3df80
durgeshahire07 <durgeshahire07@gmail.com>
2021-10-16 00:30:38 +0530

    added pre-loading animation

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
+
+
 const url = 'https://stark-plateau-00189.herokuapp.com/posts';
 
 fetch(url)
 .then(res => res.json())
 .then(data=>{
+
     // console.log(data[0]);
     // if(data[0].note){
     //     document.getElementById("note-text").innerText = `NOTE : ${data[0].note}` 
@@ -14,6 +17,9 @@ fetch(url)
     document.getElementById("avg-time-comp-text").innerText = data[0].avg_time
     document.getElementById("best-time-comp-text").innerText = data[0].best_time
     document.getElementById("worst-time-comp-text").innerText = data[0].worst_time
+    document.getElementById("loader").style.visibility = "hidden";
+    document.getElementById("main-container").style.visibility="visible"
+     
 })
 .catch(err => err);
 
